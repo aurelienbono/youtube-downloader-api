a9288a1a0d508726e530bae2d57b1b3f5800640d
aurelienbono <bonombelleaurelien08@gmail.com>
2024-12-30 17:03:39 +0000

    add google driver downloader manager

--- youtube/utils.py
+++ youtube/utils.py
@@ -2,6 +2,7 @@ import yt_dlp
 import os
 from django.conf import settings
 from uuid import uuid4
+import gdown
 
 class YoutubeDownloaderManager:
     def __init__(self):
@@ -9,27 +10,50 @@ class YoutubeDownloaderManager:
         os.makedirs(self.download_path, exist_ok=True)
 
     def download_video(self, video_url):
-        # Génération d'un nom unique pour le fichier (UUID)
         custom_name = f"{str(uuid4()).replace('-', '')}_ma_video.mp4"
         custom_path = os.path.join(self.download_path, custom_name)
 
-        # Options pour yt-dlp
         ydl_opts = {
             'format': 'mp4',
-            'outtmpl': custom_path,  # Force le chemin et le nom personnalisé
-            'restrictfilenames': True,  # Évite les caractères spéciaux ou les espaces
-            'noplaylist': True,  # Télécharge uniquement une vidéo, pas une playlist
+            'outtmpl': custom_path, 
+            'restrictfilenames': True,   
+            'noplaylist': True,  
         }
 
         try:
-            # Téléchargement avec yt-dlp
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 ydl.extract_info(video_url, download=True)
-            return custom_name  # Retourne uniquement le nom du fichier
+            return custom_name  
         except Exception as e:
             raise Exception(f"Erreur lors du téléchargement : {str(e)}")
 
     def download_mp4_video_to_link(self, video_url):
-        # Téléchargement de la vidéo
         file_name = self.download_video(video_url.strip())
         return file_name
+
+
+
+
+class GoogleDriverDownloaderManager:
+    def __init__(self):
+        pass
+    
+    def download_google_drive_file(self, file_url):
+        download_path = os.path.join(settings.MEDIA_ROOT, 'videos')
+        os.makedirs(download_path, exist_ok=True)
+        
+        custom_name = f"{str(uuid4()).replace('-', '')}_ma_video.mp4"
+        custom_path = os.path.join(download_path, custom_name)
+        
+        if "drive.google.com" in file_url and "/file/d/" in file_url:
+            file_id = file_url.split("/file/d/")[1].split("/")[0]
+            file_url = f"https://drive.google.com/uc?id={file_id}"
+        
+        gdown.download(file_url, custom_path, quiet=False)
+        
+        if custom_path.endswith('.part'):
+            final_path = custom_path.replace('.part', '.mp4')
+            os.rename(custom_path, final_path)
+            custom_name = final_path.split(os.path.sep)[-1]  
+
+        return custom_name
