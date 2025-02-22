59dc8f00258dd98f21eead3adc701e543c964386
aurelienbono <bonombelleaurelien08@gmail.com>
2025-02-22 19:07:43 +0000

    change flow

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -21,37 +21,41 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()]
 )
 
-# ✅ Headers personnalisés
-CUSTOM_HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br, zstd",
-    "Accept-Language": "en-US,en;q=0.9,fr;q=0.8",
-    "Connection": "keep-alive",
-    "Host": "ssyoutube.online",
-    "Sec-Ch-Ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "Windows",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "none",
-    "Sec-Fetch-User": "?1",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
-}
-
-# ✅ Configuration du WebDriver
-chrome_options = Options()
-chrome_options.add_argument(f"user-agent={CUSTOM_HEADERS['User-Agent']}")
-chrome_options.add_argument("--disable-infobars")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-
-logging.info("🚀 Initialisation du WebDriver Chrome...")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-logging.info("✅ WebDriver Chrome initialisé avec succès.")
-
 def youtube_downloader_full_manager(YOUTUBE_VIDEO_URL):
+        
+    # ✅ Headers personnalisés
+    CUSTOM_HEADERS = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "Accept-Encoding": "gzip, deflate, br, zstd",
+        "Accept-Language": "en-US,en;q=0.9,fr;q=0.8",
+        "Connection": "keep-alive",
+        "Host": "ssyoutube.online",
+        "Sec-Ch-Ua": '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Platform": "Windows",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+    }
+
+    # ✅ Configuration du WebDriver
+    chrome_options = Options()
+    chrome_options.add_argument(f"user-agent={CUSTOM_HEADERS['User-Agent']}")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    logging.info("🚀 Initialisation du WebDriver Chrome...")
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    logging.info("✅ WebDriver Chrome initialisé avec succès.")
+
+    
+    
+    
     try:
         download_urls = []
         downloaded_files = []
